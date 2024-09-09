3b5c7d1b5c62a048cdb4ba107d9850d50c13643c
khalil <93082942+megakal99@users.noreply.github.com>
2024-09-09 13:15:35 +0100

    Update Analyse_Echantillon_binaire_Unidimensionnel.py

--- pages/Analyse_Echantillon_binaire_Unidimensionnel.py
+++ pages/Analyse_Echantillon_binaire_Unidimensionnel.py
@@ -219,7 +219,7 @@ def binomial_test_result(population_prop,sample_prop=mean_sample,sample_size=sam
     f"✅ On ne peut pas rejeter l'hypothèse nulle H0, qui suggère que la proportion de l'échantillon ne diffère "
     f"pas de manière significative de celle de la population étudiée. Ainsi, nous ne pouvons pas conclure que "
     f"la moyenne (la proportion) de l'échantillon est significativement différente de la moyenne de la population. "
-    f"Car le risque d'erreur de rejeter à tort l'hypothèse nulle (H0) est inacceptable. Le risque est de {round(results.pvalue * 100, 2)}%.\n"        
+    f"le risque d'erreur de rejeter à tort l'hypothèse nulle (H0) étant supérieur au seuil du risque acceptable alpha ({round(results.pvalue * 100, 2)}% > {round(alpha*100,2)}%).\n"        
     f"En d'autres termes, l'échantillon reflète les principales caractéristiques dans la population, notamment la moyenne."
     f" Il est nécessaire de le confirmer à l'aide de l'analyse multidimensionnelle si possible."
     )
